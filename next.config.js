cc6029e1c1d1927e92b34d8e8519c1f93237f83a
Alex Sherman <asherman.ca@gmail.com>
2023-04-22 21:05:41 -0700

    prerender fix attempt 2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+	experimental: {
+		appDir: true,
+	},
+	target: 'serverless',
+	prerender: false,
 }
 
 module.exports = nextConfig
